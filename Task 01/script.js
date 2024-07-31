3fbc1904259f8344780e87c818e1b7afa63ab7a5
Naeem Abdullah <maliknaeemakram308@gmail.com>
2024-07-31 14:02:59 +0500

    InternShip Tasks At Programmers Tech for July InternShip

---
+++ Task 01/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute('href'));
+            window.scrollTo({
+                top: target.offsetTop - 60,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
